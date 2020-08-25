e86b506b7ca783042a466aa6f0103b483cba84dd
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-26 02:44:54 +0300

    Add timestamp naming of the result files in JSONUtil.kt

--- src/main/kotlin/org/jetbrains/research/gerritminer/model/JSONUtil.kt
+++ src/main/kotlin/org/jetbrains/research/gerritminer/model/JSONUtil.kt
@@ -2,12 +2,15 @@ package org.jetbrains.research.gerritminer.model
 
 import com.google.gson.GsonBuilder
 import java.io.File
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 const val outputPath = "output/"
 
 fun flushToFile(reviewsData: Collection<Review>, repoName: String) {
     val projectPath: String = System.getProperty("user.dir")
+    val timeStamp: String = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
     val gson = GsonBuilder().setPrettyPrinting().create()
     val jsonString = gson.toJson(reviewsData)
-    File(projectPath).resolve(outputPath + repoName).writeText(jsonString)
+    File(projectPath).resolve(outputPath + repoName + timeStamp).writeText(jsonString)
 }
