55bbc8becbacd9fb956a2896423f921d7e0df26c
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-26 02:27:39 +0300

    Add repoName CL argument
    So that results can be saved in files with related names

--- src/main/kotlin/org/jetbrains/research/gerritminer/main/Main.kt
+++ src/main/kotlin/org/jetbrains/research/gerritminer/main/Main.kt
@@ -1,17 +1,18 @@
 package org.jetbrains.research.gerritminer.main
 
 import org.jetbrains.research.gerritminer.client.Client
+import org.jetbrains.research.gerritminer.model.flushToFile
 import org.jetbrains.research.gerritminer.model.parseReviewsData
 import java.lang.Integer.parseInt
 
-const val testBaseUrl = "https://review.opendev.org"
 
 fun main(args: Array<String>) {
-    if (args.size == 2) {
+    if (args.size == 3) {
         val baseUrl = args[0]
+        val repoName = args[1]
         var limit = 0
         try {
-            limit = parseInt(args[1])
+            limit = parseInt(args[2])
         } catch (e: NumberFormatException) {
             println("Second argument must be a number! \n")
             printUsageInfo()
@@ -22,6 +23,7 @@ fun main(args: Array<String>) {
         val reviewsDataJSON = client.getResponseText(query)
         val reviewsData = parseReviewsData(reviewsDataJSON)
         println(reviewsData)
+        flushToFile(reviewsData, repoName)
     } else {
         println("Incorrect number of arguments! \n")
         printUsageInfo()
@@ -29,5 +31,5 @@ fun main(args: Array<String>) {
 }
 
 fun printUsageInfo() {
-    println("Usage: ./gradlew run --args='arg1 arg2' \nWhere arg1 - baseUrl (for instance: https://review.opendev.org) and arg2 - number of reviews to mine")
+    println("Usage: ./gradlew run --args='arg1 arg2 arg3' \nWhere arg1 - baseUrl (for instance: https://review.opendev.org), arg2 - project codename for saving results (i.e. opendev) and arg3 is the number of review to mine")
 }
