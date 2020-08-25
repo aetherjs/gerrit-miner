7d54c5a5ddf6f7235e56aadf6ce2305fac97262c
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-26 00:59:09 +0300

    Add testcase to Main.kt

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,11 +1,14 @@
 package main
 
 import client.Client
+import model.parseReviewsData
 
-
-const val testBaseUrl = "https://review.openstack.org"
+const val testBaseUrl = "https://review.opendev.org"
 
 fun main() {
     val client = Client()
-    println(client.loadReviewDiffs(testBaseUrl))
+    val query = client.constructQuery(testBaseUrl, "merged", 5)
+    val reviewsDataJSON = client.getResponseText(query)
+    val reviewsData = parseReviewsData(reviewsDataJSON)
+    println(reviewsData)
 }
