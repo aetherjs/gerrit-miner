b4945b94f3846a731ceaad7ad82137ce0c041e2c
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-26 02:26:36 +0300

    Add JSONUtil.kt with writing to JSON file functionality

--- src/main/kotlin/org/jetbrains/research/gerritminer/model/JSONUtil.kt
+++ src/main/kotlin/org/jetbrains/research/gerritminer/model/JSONUtil.kt
@@ -1,2 +1,13 @@
 package org.jetbrains.research.gerritminer.model
 
+import com.google.gson.GsonBuilder
+import java.io.File
+
+const val outputPath = "output/"
+
+fun flushToFile(reviewsData: Collection<Review>, repoName: String) {
+    val projectPath: String = System.getProperty("user.dir")
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    val jsonString = gson.toJson(reviewsData)
+    File(projectPath).resolve(outputPath + repoName).writeText(jsonString)
+}
