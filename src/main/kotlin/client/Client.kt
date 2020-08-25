e770d2b698ae0468e1c47c2a0ac983b806972839
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-25 15:08:50 +0300

    Add handle request functions

--- src/main/kotlin/client/Client.kt
+++ src/main/kotlin/client/Client.kt
@@ -1,44 +1,70 @@
 package client
 
 import khttp.get
+import khttp.responses.Response
 import model.DiffRequest
 import org.json.JSONArray
 import org.json.JSONObject
 import kotlin.random.Random
 
-fun loadReviewsData(baseUrl: String): String {
+const val REQUEST_TIMEOUT = 500
 
-    val status = "merged"
-    val limit = "5"
-    val revisionOptions = "CURRENT_REVISION"
-    val filesOptions = "CURRENT_FILES"
+class Client {
+    private var timeSinceLastRequest = 0L
+    private val lastRequestTime = 0L
 
-    val request = get("$baseUrl/changes/?q=$status&n=$limit&o=$revisionOptions&o=$filesOptions")
-    println("Request URL: ${request.url}")
-    return request.text.substringAfter(")]}'")
-}
+    private fun loadReviewsData(baseUrl: String): String {
 
-fun loadReviewDiffs(baseUrl: String): String {
-    val diffRequestInfo = mutableListOf<DiffRequest>()
+        val status = "merged"
+        val limit = "5"
+        val revisionOptions = "CURRENT_REVISION"
+        val filesOptions = "CURRENT_FILES"
 
-    val jsonObject = JSONArray(loadReviewsData(baseUrl))
-    jsonObject.forEach{
-        val changeID = (it as JSONObject).getInt("_number")
-        val revisions = (it as JSONObject).getJSONObject("revisions")
-        revisions.keys().forEach {
-            val revisionID = it
-            val files = revisions.getJSONObject(it).getJSONObject("files")
-            files.keys().forEach {path ->
-                val fileID = it
-                diffRequestInfo.add(DiffRequest(changeID, revisionID, fileID))
-            }
-        }
+        val request = get("$baseUrl/changes/?q=$status&n=$limit&o=$revisionOptions&o=$filesOptions")
+        println("Request URL: ${request.url}")
+        return request.text.substringAfter(")]}'")
     }
 
-    println(diffRequestInfo)
+    fun loadReviewDiffs(baseUrl: String): String {
+        val diffRequestInfo = mutableListOf<DiffRequest>()
 
-    val randomDiffRequest = diffRequestInfo.get(Random.nextInt(diffRequestInfo.size))
-    val response = get("$baseUrl/changes/${randomDiffRequest.changeID}/revisions/${randomDiffRequest.revisionID}/files/${randomDiffRequest.fileID}/diff")
-    println("Diff Request URL: ${response.url}")
-    return response.text
+        val jsonObject = JSONArray(loadReviewsData(baseUrl))
+        jsonObject.forEach{
+            val changeID = (it as JSONObject).getInt("_number")
+            val revisions = (it as JSONObject).getJSONObject("revisions")
+            revisions.keys().forEach {
+                val revisionID = it
+                val files = revisions.getJSONObject(it).getJSONObject("files")
+                files.keys().forEach {path ->
+                    val fileID = it
+                    diffRequestInfo.add(DiffRequest(changeID, revisionID, fileID))
+                }
+            }
+        }
+
+        println(diffRequestInfo)
+
+        val randomDiffRequest = diffRequestInfo[Random.nextInt(diffRequestInfo.size)]
+        val query = "$baseUrl/changes/${randomDiffRequest.changeID}/revisions/${randomDiffRequest.revisionID}/files/${randomDiffRequest.fileID}/diff"
+        val response = getResponse(query)
+        println("Diff Request URL: ${response.url}")
+        return response.text.substringAfter(")]}'")
+    }
+
+    fun getResponse(query: String): Response {
+        timeSinceLastRequest = System.currentTimeMillis() - lastRequestTime
+        val waitFor = REQUEST_TIMEOUT - timeSinceLastRequest
+        if (waitFor > 0) {
+            Thread.sleep(waitFor)
+        }
+        return get(query)
+    }
+
+    fun getResponseText(query: String): String {
+        val response = getResponse(query)
+        return response.text.substringAfter(")]}'")
+    }
+
+
 }
+
