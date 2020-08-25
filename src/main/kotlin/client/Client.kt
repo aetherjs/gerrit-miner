3fa06a60103142faf9bfeadb4703a2a9569ff2e9
repinsky13 <vlad.repinskiy.17@ucl.ac.uk>
2020-08-26 00:57:52 +0300

    Refactor Client.kt

--- src/main/kotlin/client/Client.kt
+++ src/main/kotlin/client/Client.kt
@@ -2,54 +2,35 @@ package client
 
 import khttp.get
 import khttp.responses.Response
-import model.DiffRequest
-import org.json.JSONArray
-import org.json.JSONObject
-import kotlin.random.Random
 
-const val REQUEST_TIMEOUT = 500
+/**
+ * API burst limit
+ */
+const val REQUEST_TIMEOUT = 500L
 
 class Client {
+
     private var timeSinceLastRequest = 0L
     private val lastRequestTime = 0L
 
-    private fun loadReviewsData(baseUrl: String): String {
-
-        val status = "merged"
-        val limit = "5"
-        val revisionOptions = "CURRENT_REVISION"
-        val filesOptions = "CURRENT_FILES"
-
-        val response = getResponse("$baseUrl/changes/?q=$status&n=$limit&o=$revisionOptions&o=$filesOptions")
-        println("Request URL: ${response.url}")
-        return response.text.substringAfter(")]}'")
+    /**
+     * Constructs a Gerrit API query from base url and optional parameters
+     */
+    fun constructQuery(baseUrl: String,
+                       status: String = "merged",
+                       limit: Int = 10,
+                       revisionOptions: String = "CURRENT_REVISION",
+                       commitOptions: String = "CURRENT_COMMIT",
+                       filesOptions: String = "CURRENT_FILES",
+                       accountsOptions: String = "DETAILED_ACCOUNTS",
+                       labelsOptions: String = "DETAILED_LABELS"): String {
+        return  "$baseUrl/changes/?q=$status&n=${limit}&o=$revisionOptions&o=$commitOptions&o=$filesOptions&o=$accountsOptions&o=$labelsOptions"
     }
 
-    fun loadReviewDiffs(baseUrl: String): String {
-        val diffRequestInfo = mutableListOf<DiffRequest>()
-
-        val jsonObject = JSONArray(loadReviewsData(baseUrl))
-        jsonObject.forEach{
-            val changeID = (it as JSONObject).getInt("_number")
-            val revisions = (it as JSONObject).getJSONObject("revisions")
-            revisions.keys().forEach {
-                val revisionID = it
-                val files = revisions.getJSONObject(it).getJSONObject("files")
-                files.keys().forEach {path ->
-                    val fileID = it
-                    diffRequestInfo.add(DiffRequest(changeID, revisionID, fileID))
-                }
-            }
-        }
-        println(diffRequestInfo)
-        val randomDiffRequest = diffRequestInfo[Random.nextInt(diffRequestInfo.size)]
-        val query = "$baseUrl/changes/${randomDiffRequest.changeID}/revisions/${randomDiffRequest.revisionID}/files/${randomDiffRequest.fileID}/diff"
-        val response = getResponse(query)
-        println("Diff Request URL: ${response.url}")
-        return response.text.substringAfter(")]}'")
-    }
-
-    fun getResponse(query: String): Response {
+    /**
+     * Implemented rate-limit throttling to avoid exhausting gerrit API resources
+     */
+    private fun getResponse(query: String): Response {
         timeSinceLastRequest = System.currentTimeMillis() - lastRequestTime
         val waitFor = REQUEST_TIMEOUT - timeSinceLastRequest
         if (waitFor > 0) {
@@ -59,11 +40,12 @@ class Client {
         return get(query)
     }
 
+    /**
+     * Returns a JSON string response from an API call
+     */
     fun getResponseText(query: String): String {
         val response = getResponse(query)
         return response.text.substringAfter(")]}'")
     }
-
-
 }
 
